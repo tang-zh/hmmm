f51cdf9525d43d2b24d7dba5dd88e988d6fb8a8a
tang <402379191@qq.com>
2020-09-22 08:31:26 +0800

    完成第一天

---
+++ src/router/index.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import login from '../views/login/index.vue'
+Vue.use(VueRouter)
+const routes = [{
+    path: '/login',
+    component: login
+}]
+const router = new VueRouter({
+    routes
+})
+export default router
---
+++ src/views/login/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="login-wrap">
+    <div class="login-box"></div>
+    <img src="./images/login_banner_ele.png" alt />
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less">
+.login-wrap {
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(225deg, #1493fa, #01c6fa);
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+
+  .login-box {
+    width: 478px;
+    height: 550px;
+    background: #f5f5f5;
+  }
+}
+</style>
